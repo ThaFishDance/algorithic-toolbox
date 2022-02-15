46a911735a79c72d6364b51ab144f41d4ee01430
chuff <chuffman91@gmail.com>
2022-02-15 06:05:21 -0600

    Add a remove word method to Trie

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         var trie = new Trie();
-        trie.insert("cat");
-        System.out.println();
-        System.out.println(trie.contains("cat"));
+        trie.insert("car");
+        trie.insert("care");
+        trie.delete("car");
+        trie.contains("car");
         System.out.println("Done");
     }
 }
--- src/com/company/Trie.java
+++ src/com/company/Trie.java
@@ -3,6 +3,7 @@ package com.company;
 import java.util.HashMap;
 
 public class Trie {
+
     private class TrieNode {
         char value;
         private HashMap<Character, TrieNode> children = new HashMap<>();
@@ -22,6 +23,10 @@ public class Trie {
             return children.containsKey(ch);
         }
 
+        public boolean hasChildren() {
+            return !children.isEmpty();
+        }
+
         // addChild -> void
         public void addChild(Character ch) {
             children.put(ch, new TrieNode(ch));
@@ -29,6 +34,14 @@ public class Trie {
 
         public TrieNode getChild(char ch) {
             return children.get(ch);
+        }
+
+        public TrieNode[] getChildren() {
+            return children.values().toArray(new TrieNode[0]);
+        }
+
+        public void removeChild(char ch) {
+            children.remove(ch);
         }
     }
 
@@ -45,16 +58,63 @@ public class Trie {
         current.isEndOfWord = true;
     }
 
-    public boolean contains(String word){
-        if(word == null) return false;
+    public boolean contains(String word) {
+        if (word == null) return false;
 
         var current = root;
-        for(var ch: word.toCharArray()){
-            if(!current.hasChild(ch)){
+        for (var ch : word.toCharArray()) {
+            if (!current.hasChild(ch)) {
                 return false;
             }
             current = current.getChild(ch);
         }
         return current.isEndOfWord;
+    }
+
+    public void preTraverse() {
+        preTraverse(root);
+    }
+
+    private void preTraverse(TrieNode node) {
+        System.out.println(node.value);
+        for (var child : node.getChildren()) {
+            preTraverse(child);
+        }
+    }
+
+    public void postTraverse() {
+        postTraverse(root);
+    }
+
+    private void postTraverse(TrieNode node) {
+        for (var child : node.getChildren()) {
+            postTraverse(child);
+        }
+        System.out.println(node.value);
+    }
+
+    public void delete(String word) {
+        if (word == null) return;
+        delete(root, word, 0);
+    }
+
+    private void delete(TrieNode node, String word, int index) {
+
+        if (index == word.length()) {
+            node.isEndOfWord = false;
+            // cannot remove node from trie on itself.
+            // Need to remove while on parent
+            return;
+        }
+        char ch = word.charAt(index);
+        TrieNode child = node.getChild(ch);
+        if (child == null) return;
+        delete(child, word, index + 1);
+        // on parent, can remove child
+        // if parent does not have other children or not endOfWord, you can remove child.
+        if (!child.hasChildren() && !child.isEndOfWord) {
+            root.removeChild(ch);
+        }
+
     }
 }
